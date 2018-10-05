3b0cb8bd93de2fc10fd83b2ebd55f7747558f64a
tlifschitz <tlifschitz@itba.edu.ar>
2018-10-05 13:59:37 -0300

    FTM

---
+++ Modem FSK/source/FTM.h
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                     				                                    	   //
+//          Grupo 3 - Laboratorio de Microprocesadores - ITBA - 2018           //
+//	                                                                           //
+/////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef FTM_H_
+#define FTM_H_
+
+// Los que tienen * son los que se pueden acceder en el header
+
+/*
+PTB12 Alt3: FTM1_CH0 Alt4: FTM0_CH4 Alt6: FTM1_QD_PHA
+PTB13 Alt3: FTM1_CH1 Alt4: FTM0_CH5 Alt6: FTM1_QD_PHB
+
+PTE5  Alt6: FTM3_CH0
+PTE6  Alt6: FTM3_CH1
+PTE7  Alt6: FTM3_CH2
+PTE8  Alt6: FTM3_CH3
+PTE9  Alt6: FTM3_CH4
+PTE10 Alt6: FTM3_CH5
+PTE11 Alt6: FTM3_CH6
+PTE12 Alt6: FTM3_CH7
+
+PTA0  Alt3: FTM0_CH5
+PTA1  Alt3: FTM0_CH6  *
+PTA2  Alt3: FTM0_CH7  *
+PTA3  Alt3: FTM0_CH0
+PTA4  Alt3: FTM0_CH1
+PTA5  Alt3: FTM0_CH2
+PTA6  Alt3: FTM0_CH3
+PTA7  Alt3: FTM0_CH4
+PTA8  Alt3: FTM1_CH0
+PTA9  Alt3: FTM1_CH1
+PTA10  Alt3: FTM2_CH0
+PTA11  Alt3: FTM2_CH1
+PTA12  Alt3: FTM1_CH0
+PTA13  Alt3: FTM1_CH1
+*/
+#endif /* FTM_H_ */
