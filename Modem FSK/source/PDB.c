8e25e6c6c2e5f85ae86fa2de2ce0ff80e90fa5f0
tlifschitz <tlifschitz@itba.edu.ar>
2018-10-17 21:23:00 -0300

    PDB

---
+++ Modem FSK/source/PDB.c
@@ -0,0 +1,112 @@
+#include "PDB.h"
+#include "MK64F12.h"
+
+
+void PDB_GetDefaultConfig(PDB_Config * config)
+{
+	config->loadValueMode = PDB_IMMEDIATELY;
+    config->prescalerDivider = PDB_PRESCALER_DIVIDER_1;
+    config->multiplicationFactor = PDB_MULT_FACTOR_1;
+    config->triggerInputSource = PDB_TRIGGER_SOFTWARE;
+    config->enableContinuousMode = false;
+}
+
+void PDB_Init(PDB_Config * config)
+{
+	// Enable clock
+	SIM->SCGC6 |= SIM_SCGC6_PDB_MASK;
+
+	PDB0->SC = PDB_SC_LDMOD(config->loadValueMode) |
+			PDB_SC_PRESCALER(config->prescalerDivider) |
+			PDB_SC_TRGSEL(config->triggerInputSource) |
+			PDB_SC_MULT(config->multiplicationFactor);
+
+	if(config->enableContinuousMode==true)
+		PDB0->SC |= PDB_SC_CONT_MASK;
+
+	// Enable PDB
+	PDB0->SC |= PDB_SC_PDBEN_MASK;
+}
+
+void PDB_Deinit()
+{
+	// Disable PDB
+	PDB0->SC &= ~PDB_SC_PDBEN_MASK;
+
+	// Disable clock
+	SIM->SCGC6 &= ~SIM_SCGC6_PDB_MASK;
+}
+
+void PDB_Enable()
+{
+	// Enable PDB
+	PDB0->SC |= PDB_SC_PDBEN_MASK;
+}
+
+void PDB_Disable()
+{
+	// Disable PDB
+	PDB0->SC &= ~PDB_SC_PDBEN_MASK;
+
+}
+
+void PDB_Trigger()
+{
+	PDB0->SC |= PDB_SC_SWTRIG_MASK;
+}
+
+void PDB_LoadValues()
+{
+	PDB0->SC |= PDB_SC_LDOK_MASK;
+}
+
+void PDB_EnableDMA(bool enable)
+{
+	PDB0->SC |= PDB_SC_DMAEN_MASK;
+}
+
+void PDB_EnableInterrupts(uint32_t mask)
+{
+	PDB0->SC |= PDB_SC_PDBIE_MASK;
+}
+
+void PDB_DisableInterrupts(uint32_t mask)
+{
+	PDB0->SC &= ~PDB_SC_PDBIE_MASK;
+}
+
+
+uint32_t PDB_GetStatusFlags();
+
+void PDB_ClearStatusFlags(uint32_t mask);
+
+void PDB_SetModulusValue(uint32_t value)
+{
+	PDB0->MOD = PDB_MOD_MOD(value);
+}
+
+uint32_t PDB_GetCounterValue()
+{
+	return (PDB0->CNT&PDB_CNT_CNT_MASK);
+}
+
+void PDB_SetCounterDelayValue(uint32_t value)
+{
+	PDB0->IDLY = PDB_IDLY_IDLY(value);
+}
+
+void PDB_SetDacTrigger(uint16_t value)
+{
+	// Disable external trigger
+	PDB0->DAC[0].INTC &= ~PDB_INTC_EXT(0);
+	// Set interval value for DAC trigger
+	PDB0->DAC[0].INT = PDB_INT_INT(value);
+}
+
+void PDB_EnableDacTriger(bool enable)
+{
+	if(enable)
+		PDB0->DAC[0].INTC |= PDB_INTC_TOE_MASK;
+	else
+		PDB0->DAC[0].INTC &= ~PDB_INTC_TOE_MASK;
+}
---
+++ Modem FSK/source/PDB.h
@@ -0,0 +1,110 @@
+#ifndef PDB_H_
+#define PDB_H_
+
+#include "stdbool.h"
+#include "stdint.h"
+
+typedef enum {
+  PDB_IMMEDIATELY,
+  PDB_ON_COUNTER_OVERFLOW,
+  PDB_ON_TRIGGER_INPUT,
+  PDB_ON_COUNTER_OVERFLOW_OR_TRIGGER_INPUT
+}PDB_LoadValueMode;
+
+typedef enum {
+  PDB_PRESCALER_DIVIDER_1 = 0U,
+  PDB_PRESCALER_DIVIDER_2 = 1U,
+  PDB_PRESCALER_DIVIDER_4 = 2U,
+  PDB_PRESCALER_DIVIDER_8 = 3U,
+  PDB_PRESCALER_DIVIDER_16 = 4U,
+  PDB_PRESCALER_DIVIDER_32 = 5U,
+  PDB_PRESCALER_DIVIDER_64 = 6U,
+  PDB_PRESCALER_DIVIDER_128 = 7U
+}PDB_Prescaler;
+
+typedef enum {
+  PDB_MULT_FACTOR_1 = 0U,
+  PDB_MULT_FACTOR_10 = 1U,
+  PDB_MULT_FACTOR_20 = 2U,
+  PDB_MULT_FACTOR_40 = 3U
+}PDB_MultiplicationFactor;
+
+typedef enum {
+  PDB_TRIGGER_INPUT_0 = 0U,
+  PDB_TRIGGER_INPUT_1 = 1U,
+  PDB_TRIGGER_INPUT_2 = 2U,
+  PDB_TRIGGER_INPUT_3 = 3U,
+  PDB_TRIGGER_INPUT_4 = 4U,
+  PDB_TRIGGER_INPUT_5 = 5U,
+  PDB_TRIGGER_INPUT_6 = 6U,
+  PDB_TRIGGER_INPUT_7 = 7U,
+  PDB_TRIGGER_INPUT_8 = 8U,
+  PDB_TRIGGER_INPUT_9 = 9U,
+  PDB_TRIGGER_INPUT_10 = 10U,
+  PDB_TRIGGER_INPUT_11 = 11U,
+  PDB_TRIGGER_INPUT_12 = 12U,
+  PDB_TRIGGER_INPUT_13 = 13U,
+  PDB_TRIGGER_INPUT_14 = 14U,
+  PDB_TRIGGER_SOFTWARE = 15U
+}PDB_TriggerSource;
+
+typedef struct{
+	PDB_LoadValueMode loadValueMode;
+	PDB_Prescaler prescalerDivider;
+	PDB_MultiplicationFactor multiplicationFactor;
+	PDB_TriggerSource triggerInputSource;
+	bool enableContinuousMode;
+}PDB_Config;
+
+void PDB_GetDefaultConfig(PDB_Config * config);
+
+void PDB_Init(PDB_Config * config);
+
+void PDB_Deinit();
+
+void PDB_Enable();
+
+void PDB_Disable();
+
+void PDB_Trigger();
+
+void PDB_LoadValues();
+
+void PDB_EnableDMA(bool enable);
+
+void PDB_EnableInterrupts(uint32_t mask);
+
+void PDB_DisableInterrupts(uint32_t mask);
+
+uint32_t PDB_GetStatusFlags();
+
+void PDB_ClearStatusFlags(uint32_t mask);
+
+/**
+ * @brief Specifies the period of the counter.
+ * When the counter reaches this value, it will be reset back to zero.
+ * If the PDB is in Continuous mode, the count begins anew.
+ */
+void PDB_SetModulusValue(uint32_t value);
+
+/**
+ *
+ */
+uint32_t PDB_GetCounterValue();
+
+/**
+ *
+ */
+void PDB_SetCounterDelayValue(uint32_t value);
+
+/**
+ *
+ */
+void PDB_SetDacTriggerPeriod(uint16_t value);
+
+/**
+ *
+ */
+void PDB_EnableDacTriger(bool enable);
+
+#endif /* PDB_H_ */
