5edbf3e4d45f829be1b297ba9209aa23827c82dd
tlifschitz <tlifschitz@itba.edu.ar>
2018-10-26 08:52:42 -0300

    Buffer circular de floats para hacer el filtrado!

---
+++ Modem FSK/source/FloatBuffer.h
@@ -0,0 +1,58 @@
+#ifndef FLOATBUFFER_H_
+#define FLOATBUFFER_H_
+
+// Head points always to the oldest value. Buffer is never full, oldest value is overriden.
+// 			 __________________________________________________________
+//			|  tail   |         |          |            |	  head    |
+//			| 	n     |	  n-1   |   n-2    | 	n-3     |   n-(size-1)|
+//			 ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+// In memory:
+// 			 __________________________________________________________
+//			|         |         |   tail-> |    head->  |	          |
+//			| 	n-2   |	  n-1   |     n    | 	n-4     |     n-3     |
+//			 ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+
+typedef struct{
+	float * data;
+	uint32_t head,tail;
+	uint32_t size;
+}FloatBuffer;
+
+/**
+*
+*/
+#define NEW_FLOAT_BUFFER(name,_size) \
+	static float name##_data[_size]; \
+	static FloatBuffer name = {	.data= name##_data, \
+								.head=0, 			\
+								.tail=0,			\
+								.size=_size          }
+
+/**
+*
+*/
+#define PUSH(name,value) \
+	name.data[name.head] = value;			\
+	name.head = (name.head + 1) % name.size
+
+/**
+*	Return tail + i element
+*	Note: GET(name,i) == GET(name,i+size)
+*/
+#define GET(name,i)       \
+	name.data[ (name.size+name.head-(i+1)) % name.size ]
+
+/**
+*	Return oldest element
+*/
+#define GET_HEAD(name) \
+	name.data[name.head]
+
+/**
+*	Return last element
+*/
+#define GET_TAIL(name)\
+	name.data[(name.size+name.head-1)%name.size]
+
+
+#endif /* FLOATBUFFER_H_ */
